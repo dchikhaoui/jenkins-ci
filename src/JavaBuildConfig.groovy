c2938442c91e32653ef7429c6056b304d024a791
sybrenbolandit <sybrenbolandit@gmail.com>
2019-02-05 20:54:39 +0100

    Setup pipeline library

---
+++ src/JavaBuildConfig.groovy
@@ -0,0 +1,4 @@
+class JavaBuildConfig {
+    String imageTag = 'sybrenbolandit/app'
+    String mavenImage = 'maven:3.5-jdk-8'
+}
---
+++ vars/javaApplicationPipeline.groovy
@@ -0,0 +1,60 @@
+#!/usr/bin/groovy
+
+def call(config = [:]) {
+    config = config as JavaBuildConfig
+
+    podTemplate(
+            label: 'slave-pod',
+            inheritFrom: 'default',
+            containers: [
+                    containerTemplate(name: 'maven', image: config.mavenImage, ttyEnabled: true, command: 'cat'),
+                    containerTemplate(name: 'docker', image: 'docker:18.02', ttyEnabled: true, command: 'cat'),
+                    containerTemplate(name: 'kubectl', image: 'traherom/kustomize-docker:1.0.5', ttyEnabled: true, command: 'cat')
+            ],
+            volumes: [
+                    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                    hostPathVolume(hostPath: '/root/.m2', mountPath: '/root/.m2')
+            ]
+    ) {
+        node('slave-pod') {
+            try {
+                def commitId
+                stage ('Extract') {
+                    checkout scm
+                    commitId = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+                }
+
+                stage ('Build') {
+                    container('maven') {
+                        sh 'mvn clean install'
+                    }
+                }
+
+                stage ('Docker build and push') {
+                    container ('docker') {
+                        withCredentials([usernamePassword(credentialsId: 'dockerhub',
+                                usernameVariable: 'registryUser', passwordVariable: 'registryPassword')]) {
+
+                            sh "docker login -u=$registryUser -p=$registryPassword"
+                            sh "docker build -t $config.imageTag:$commitId ."
+                            sh "docker push $config.imageTag:$commitId"
+                        }
+                    }
+                }
+
+                stage ("Deploy") {
+                    container ('kubectl') {
+                        dir ("deployment") {
+                            sh """
+                                kustomize edit set imagetag $config.imageTag:$commitId;
+                                kustomize build overlays/test | kubectl apply --record -f  -
+                            """
+                        }
+                    }
+                }
+            } finally {
+                cleanWs()
+            }
+        }
+    }
+}
